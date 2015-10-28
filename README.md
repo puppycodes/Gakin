bd2a3fddefe1e47e40d3e588ad0d6b7ed01a5475
XAMPP <vijvanne@uat.edu>
2015-10-28 09:56:20 -0400

    Initial Commit, still janky

---
+++ README.md
@@ -0,0 +1,2 @@
+# Gakin
+TODO
---
+++ main.go
@@ -0,0 +1,112 @@
+/*
+	Gakin IRC Webhook bot
+*/
+package main
+
+import (
+	"fmt"
+	"net/url"
+	"net/http"
+	"io/ioutil"
+	"github.com/jeffail/gabs"
+	irc "github.com/fluffle/goirc/client"
+)
+
+var message = make(chan string);
+
+func GitioShort(_url string) (string) {
+	resp, err  := http.PostForm("http://git.io", url.Values{"url": {_url}});
+	if err != nil {
+		fmt.Printf("[*] GitioShort error: %s\n", err.Error());
+	}
+	defer resp.Body.Close();
+	return resp.Header.Get("Location");
+}
+
+
+func ProcessEvent(data *gabs.Container, event string) {
+	switch event {
+	case "issue":
+
+	case "push":
+
+	case "pull_request":
+
+	default:
+		fmt.Printf("[*] HOOK %s\n", event);
+	}
+}
+
+func HandlePost(w http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case "POST":
+		body, err := ioutil.ReadAll(req.Body);
+		if err != nil {
+			fmt.Printf("[*] HandlePost error: %s\n", err.Error());
+		}
+		r, err := gabs.ParseJSON(body);
+		if err != nil {
+			fmt.Printf("[*] HandlePost error: %s\n", err.Error());
+		}
+		ProcessEvent(r, req.Header.Get("X-Github-Event"));
+	default:
+		w.Header().Set("Content-Type", "text/html");
+		fmt.Fprintf(w, "<center><h1>Shhhhhhhhhhhhhhh</h1><br /><object width='800' height='600' data='http://archive.bad-alloc.net/other/flash/garrett.swf'></object></center>");
+	}
+}
+
+func main() {
+
+	http.HandleFunc("/", HandlePost);
+
+	go IRCConnection("chat.freenode.net", "##XAMPP");
+	http.ListenAndServe(":9987", nil);
+}
+
+
+
+func IRCConnection(host string, channel string) {
+	IRCConnQuit := make(chan bool);
+	run := true;
+	cfg := irc.NewConfig("Gakin", "Gakin");
+    cfg.Server = host;
+    cfg.NewNick = func(n string) string { return n + "~" };
+    cli := irc.Client(cfg);
+
+    cli.EnableStateTracking();
+
+	cli.HandleFunc(irc.DISCONNECTED, func(conn *irc.Conn, line *irc.Line) {
+		fmt.Printf("[*] Connect Done\n");
+		IRCConnQuit <- true;
+		run = false;
+
+	});
+	cli.HandleFunc(irc.PRIVMSG, func(conn *irc.Conn, line *irc.Line) {
+		fmt.Printf("[*] %s\n", line.Text());
+	});
+	cli.HandleFunc(irc.NOTICE, func(conn *irc.Conn, line *irc.Line) {
+		fmt.Printf("[*] %s\n", line.Text());
+	});
+	cli.HandleFunc(irc.TOPIC, func(conn *irc.Conn, line *irc.Line) {
+		fmt.Printf("[*] %s\n", line.Text());
+	});
+
+	cli.HandleFunc(irc.CONNECTED, func(conn *irc.Conn, line *irc.Line) {
+		fmt.Printf("[*] Joining %s\n", channel);
+		cli.Join(channel);
+	})
+
+	fmt.Printf("[*] Connecting to %s\n", host);
+	if err := cli.Connect(); err != nil {
+		fmt.Printf("[*] Connection error: %s\n", err.Error());
+	}
+
+	// Run Worker
+	for run {
+		strmsg := <- message;
+		fmt.Printf("[!] %s\n", strmsg);
+
+	}
+
+	<- IRCConnQuit;
+}
